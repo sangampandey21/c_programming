444ed3cbf0e02b51eb3aeaff2b10acb22e8795f9
sangampandey21 <147520794+sangampandey21@users.noreply.github.com>
2024-08-06 22:59:26 +0530

    Create HelloWorld.c
    committing first  Hello World program

---
+++ C course/HelloWorld.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+
+    printf("Hello World! What's up ?\n");
+    printf("This is me Sangam , an aspiring software developer and this is my first program.\n");
+
+    return 0;
+}
