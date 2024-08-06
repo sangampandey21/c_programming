3bc5a2b9430c09f66344472583c1accc9374e978
sangampandey21 <147520794+sangampandey21@users.noreply.github.com>
2024-08-06 23:06:25 +0530

    Update HelloWorld.c
    updated msg

--- C course/HelloWorld.c
+++ C course/HelloWorld.c
@@ -3,7 +3,7 @@
 int main() {
 
     printf("Hello World! What's up ?\n");
-    printf("This is me Sangam , an aspiring software developer and this is my first program.\n");
+    printf("This is me Sangam Pandey, an aspiring software developer and this is my first program.\n");
 
     return 0;
 }
